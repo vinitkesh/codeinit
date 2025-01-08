927ee25d973ff0904596c14e1266c4037f1891ee
Vinit Keshri <vinitkehri4171@gmail.com>
2025-01-08 11:09:20 +0530

    fix: Overlap in mobile

--- src/App.jsx
+++ src/App.jsx
@@ -49,22 +49,17 @@ useEffect(() => {
 
   gsap.fromTo(dates.current, {opacity: 0, scale: 0 }, {opacity: 1, scale: 1 , duration: 0.5, delay: 1.5});
 
-  // gsap.to(refLogo1.current, {opacity: 0, scale: 0, y: '100%', duration: 0.5, delay: 2.5});
-  // gsap.to(refLogo2.current, {opacity: 0, scale: 0, y: '100%', duration: 0.5, delay: 2.4});
-  // gsap.to(refLogo3.current, {opacity: 0, scale: 0, y: '100%', duration: 0.5, delay: 2.3});
-  // gsap.to(refLogo4.current, {opacity: 0, scale: 0, y: '100%', duration: 0.5, delay: 2.2});
-
 }
 , []);
 
   return (
-    <div className="min-h-screen w-screen flex items-center justify-center bg-neutral-800 relative overflow-clip m-0 p-0">
-      <div className="logo absolute top-3 h-20 w-20 font-mono text-white flex flex-col items-center gap-2 text-xl z-0">
-        <img ref={text1} src="/images/CSEALogo.svg " alt=""  className="object-cover w-full h-full"/>
-        <div ref={text2} className="span">Presents</div>
+    <div className="min-h-screen max-h-[130vh] w-screen flex flex-col items-center justify-center bg-neutral-800 relative overflow-clip m-0 p-0">
+      <div className="logo h-max font-mono text-white flex flex-col items-center gap-2 text-xl z-0">
+        <img ref={text1} src="/images/CSEALogo.svg " alt=""  className="object-cover h-20 w-20 "/>
+        <div ref={text2} className="span font-pixeloperator ">Presents</div>
       </div>
 
-      <div className="flex flex-col absolute w-full items-center justify-center h-max gap-1 z-0 ">
+      <div className="flex flex-col absolute w-full items-center justify-center h-max gap-1 z-0 text ">
         <div ref={refLogo1} className="w-[90%] lg:w-[80%] h-auto flex justify-center  "><img src="/images/LogoFill.svg" alt="" className="object-cover" /></div>
         <div ref={refLogo2} className="w-[90%] lg:w-[80%] h-auto flex justify-center  "><img src="/images/LogoNoFill.svg" alt="" className="object-cover" /></div>
         <div ref={refLogo3} className="w-[90%] lg:w-[80%] h-auto flex justify-center  "><img src="/images/LogoNoFill.svg" alt="" className="object-cover" /></div>
@@ -75,16 +70,16 @@ useEffect(() => {
         <div ref={refLogo8} className="w-[90%] lg:w-[80%] h-auto flex justify-center lg:hidden "><img src="/images/LogoNoFill.svg" alt="" className="object-cover" /></div>
      </div>
 
-     <div ref={Laptop} className="absolute h-max w-max z-10 md:top-20"><img src="/images/Laptop2.png" alt="" className="" /></div>
-     <a ref={qr} href='https://www.google.com' className="absolute md:flex hidden cursor-pointer h-max w-max origin-bottom bottom-8 left-2 z-0 "><img src="/images/QR.svg" alt="" className="" /></a>
-     <div ref={dates} className="absolute md:flex hidden h-max w-max origin-bottom bottom-12 right-2 "><img src="/images/Dates.svg" alt="" className=" w-[250px] md:w-[350px]" /></div>
+     <div ref={Laptop} className=" h-max w-max z-20 top-20 bottom-0 md:top-0"><img src="/images/Laptop2.png" alt="" className="" /></div>
+     {/* <a ref={qr} href='https://www.google.com' className="absolute md:flex hidden cursor-pointer h-max w-max origin-bottom bottom-8 left-2 z-0 "><img src="/images/QR.svg" alt="" className="" /></a>
+     <div ref={dates} className="absolute md:flex hidden h-max w-max origin-bottom bottom-12 right-2 "><img src="/images/Dates.svg" alt="" className=" w-[250px] md:w-[350px]" /></div> */}
 
-    <div className="appear flex flex-col absolute h-max bottom-10 md:hidden z-0 gap-5">
+    <div className="appear flex flex-col md:flex-row h-max w-full md:justify-between p-3 bottom-10 items-center z-0 gap-5">
       <a href='https://www.google.com' className=" cursor-pointer h-max w-max origin-bottom bottom-8 left-2 "><img src="/images/QR.svg" alt="" className="" /></a>
       <div ref={dates} className=" h-max w-max origin-bottom bottom-12 right-2 "><img src="/images/Dates.svg" alt="" className=" w-[250px] md:w-[350px]" /></div>
     </div>
 
-     <div className="bg-[#01FF68] w-full h-8 absolute bottom-0 z-0"></div>
+     {/* <div className="bg-[#01FF68] w-full h-8 absolute bottom-0 z-0"></div> */}
 
      <div className="absolute left-5 md:w-[50%] w-[100%] z-0"><img src="/images/code.svg" alt="" className="w-full -z-10 opacity-15" /></div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", "system-ui"],
+        pixeloperator: ["PixelOperator", "monospace"],
+      },
+    },
   },
   plugins: [],
 }
