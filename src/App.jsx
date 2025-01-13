8aeafd5461635d18e60554d4351154496546aa83
Vinit Keshri <vinitkehri4171@gmail.com>
2025-01-14 00:34:25 +0530

    Added fosscell

--- src/App.jsx
+++ src/App.jsx
@@ -55,10 +55,13 @@ useEffect(() => {
 , []);
 
   return (
-    <div className="min-h-screen max-h-[150vh] w-screen flex flex-col items-center justify-center bg-neutral-800 relative overflow-clip m-0 p-0">
+    <div className="min-h-screen max-h-[150vh] maxw-screen flex flex-col items-center justify-center bg-neutral-800 relative overflow-clip m-0 p-0">
       <div className="logo h-max font-mono text-white flex flex-col items-center gap-2 text-xl z-0">
-        <img ref={text1} src="https://raw.githubusercontent.com/vinitkesh/codeinit/26c7faa6b42fc08320e2bcfd8c714b295c74aed6/public/images/CSEALogo.svg " alt=""  className="object-cover h-20 w-20 "/>
-        <div ref={text2} className="span font-pixeloperator ">Presents</div>
+      <div className="flex items-center justify-center">
+        <img src="https://raw.githubusercontent.com/vinitkesh/codeinit/26c7faa6b42fc08320e2bcfd8c714b295c74aed6/public/images/CSEALogo.svg " alt=""  className="object-cover h-20 w-20 "/>
+        <img src="/images/fosscell.svg " alt=""  className="object-cover h-16 "/>
+      </div>
+      <div  className="span font-pixeloperator ">Presents</div>
       </div>
 
       <div className="flex flex-col absolute w-full items-center justify-center h-max gap-1 z-0 text ">
@@ -72,16 +75,16 @@ useEffect(() => {
         <div ref={refLogo8} className="w-[90%] lg:w-[80%] h-auto flex justify-center lg:hidden "><img src="https://raw.githubusercontent.com/vinitkesh/codeinit/26c7faa6b42fc08320e2bcfd8c714b295c74aed6/public/images/LogoNoFill.svg" alt="" className="object-cover" /></div>
      </div>
 
-     <div ref={Laptop} className=" h-max w-max z-20 top-20 bottom-0 md:top-0"><img src="https://raw.githubusercontent.com/vinitkesh/codeinit/26c7faa6b42fc08320e2bcfd8c714b295c74aed6/public/images/Laptop2.png" alt="" className="" /></div>
-     {/* <a ref={qr} href='https://www.google.com' className="absolute md:flex hidden cursor-pointer h-max w-max origin-bottom bottom-8 left-2 z-0 "><img src="https://raw.githubusercontent.com/vinitkesh/codeinit/26c7faa6b42fc08320e2bcfd8c714b295c74aed6/public/images/QR.svg" alt="" className="" /></a>
-     <div ref={dates} className="absolute md:flex hidden h-max w-max origin-bottom bottom-12 right-2 "><img src="https://raw.githubusercontent.com/vinitkesh/codeinit/26c7faa6b42fc08320e2bcfd8c714b295c74aed6/public/images/Dates.svg" alt="" className=" w-[250px] md:w-[350px]" /></div> */}
+     <div  className=" z-20 flex items-center justify-end">
+        <img ref={Laptop} src="https://raw.githubusercontent.com/vinitkesh/codeinit/26c7faa6b42fc08320e2bcfd8c714b295c74aed6/public/images/Laptop2.png" alt="" className="" />
+     </div>
 
     <div className="appear flex flex-col md:flex-row h-max w-full md:justify-between p-3 bottom-10 items-center z-10 gap-5">
       <a href={'https://docs.google.com/forms/d/e/1FAIpQLScxSEV-ADCJ9EIcmSWhBNO8E9m-ko-P4NrHrs1E7j_k-TxfEw/viewform?usp=header'} 
          className=" cursor-pointer hover:-translate-y-1 transition-transform h-max w-max origin-bottom bottom-8 left-2 ">
         <img src="https://raw.githubusercontent.com/vinitkesh/codeinit/26c7faa6b42fc08320e2bcfd8c714b295c74aed6/public/images/QR2.svg" alt="" className="" />
       </a>
-      <div ref={dates} className=" h-max w-max origin-bottom bottom-12 right-2 "><img src="https://raw.githubusercontent.com/vinitkesh/codeinit/26c7faa6b42fc08320e2bcfd8c714b295c74aed6/public/images/Dates3.svg" alt="" className=" w-[250px] md:w-[350px]" /></div>
+      <div ref={dates} className=" h-max w-max origin-bottom bottom-12 right-2 "><img src="https://raw.githubusercontent.com/vinitkesh/codeinit/833adbae216c774e39572babc620a0b5ad75dfdd/public/images/Dates3.svg" alt="" className=" w-[250px] md:w-[350px]" /></div>
     </div>
 
      {/* <div className="bg-[#01FF68] w-full h-8 absolute bottom-0 z-0"></div> */}
